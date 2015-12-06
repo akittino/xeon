90c7129d2018603d85d619d4b421c8ef8d5865db
Aleksander Kantak <kanti1992@gmail.com>
2015-12-06 18:45:56 +0100

    initial

---
+++ daxnoopt.c
@@ -0,0 +1,138 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <omp.h>
+ 
+#define size 10000
+#define INT_MAX (unsigned int)-1
+int allNodes[size][size];
+ 
+void generateGraph()
+{
+        struct timeval tvalBefore;
+        struct timeval tvalAfter;
+ 
+        //time_t t;
+        int i, j, k, randomData, column, row;
+        //srand((unsigned)time(&t));
+ 
+        gettimeofday(&tvalBefore, NULL);
+ 
+        for (i = 0; i < size; i++)
+        {
+ 
+                for (j = i; j < size; j++)
+                {
+                        if (i == j)
+                        {
+ 
+                                allNodes[i][j] = 0;
+                        }
+                        else
+                        {
+ 
+                                randomData = rand();
+ 
+                                allNodes[i][j] = randomData;
+                                allNodes[j][i] = randomData;
+                        }
+                }
+        }
+        column = 0;
+        row = 0;
+ 
+        for (k = 0; k < size; k++)
+        {
+                column = rand() % size;
+                row = rand() % size;
+                allNodes[column][row] = 0;
+                allNodes[row][column] = 0;
+ 
+                //printf("column : %d, row: %d\n", column, row);
+        }
+ 
+        gettimeofday(&tvalAfter, NULL);
+        printf("Time in microseconds: %ld microseconds\n",
+                ((tvalAfter.tv_sec - tvalBefore.tv_sec) * 1000000L
+                        + tvalAfter.tv_usec) - tvalBefore.tv_usec
+                );
+ 
+}
+ 
+void countDijkstr(int src, int dst)
+{
+ 
+        struct timeval tvalBefore;
+        struct timeval tvalAfter;  
+        unsigned int shortestPath[size];
+        char visited[size];
+        unsigned int min, indexOfMin;
+        int i, j, k, m;
+        int numberOfThreads;
+ 
+        gettimeofday(&tvalBefore, NULL);
+//#pragma omp parallel for
+        for (i = 0; i < size; i++)
+        {
+                shortestPath[i] = INT_MAX;
+                visited[i] = 0;
+        }
+        shortestPath[src] = 0;
+ 
+        for (m = 0; m < size; m++) {
+                min = INT_MAX;
+ 
+//#pragma omp parallel for
+                for (k = 0; k < size; k++)
+                {
+                        if (visited[k] == 0 && shortestPath[k] <= min)
+                        {
+                                min = shortestPath[k];
+                                indexOfMin = k;
+ 
+                        }
+                }
+                visited[indexOfMin] = 1;
+ 
+//#pragma omp parallel for
+                for (j = 0; j < size; j++)
+                {
+                        if (visited[j] == 0 && allNodes[indexOfMin][j] != 0 && (shortestPath[indexOfMin] + allNodes[indexOfMin][j]) <= shortestPath[j])
+                        {
+                                shortestPath[j] = allNodes[indexOfMin][j] + shortestPath[indexOfMin];
+                        }
+                }
+        }
+ 
+        gettimeofday(&tvalAfter, NULL);
+        printf("Time in microseconds: %ld microseconds\n",
+                ((tvalAfter.tv_sec - tvalBefore.tv_sec) * 1000000L
+                        + tvalAfter.tv_usec) - tvalBefore.tv_usec
+                );
+        printf("Shortest Path: %d", shortestPath[dst]);
+ 
+}
+ 
+void printGraph()
+{
+        int i, j;
+        for (i = 0; i < size; i++)
+        {
+                for (j = 0; j < size; j++)
+                {
+                        printf("%d ", allNodes[i][j]);
+                }
+                printf("\n");
+        }
+}
+ 
+int main()
+{
+        generateGraph();
+        printf("Graph generated\n");
+        //printGraph();
+ 
+        countDijkstr(0, size - 1);
+ 
+        return 0;
+}
---
+++ mstnoopt.c
@@ -0,0 +1,159 @@
+// scp mst.c student01@apl12.eti.pg.gda.pl:~/kantee
+// source /opt/intel/composer_xe_2013_sp1.3.174/bin/compilervars.sh intel64
+// icc -openmp -mmic -O3 mst.c -o mst
+// scp mst mic0:~
+// scp /opt/intel/composer_xe_2013_sp1.3.174/compiler/lib/mic/libiomp5.so mic0:~
+// export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:.
+// time ./mst
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <omp.h>
+
+typedef short unsigned int dint;
+
+#define SIZE 1000
+#define MAX (dint)-1
+#define TRUE 1
+
+dint data[SIZE][SIZE];
+short got[SIZE];
+
+void generateGraph()
+{
+    unsigned ii, jj;
+//#pragma omp parallel for // - not used to have always the same data
+// unused
+    for (ii = 0; ii < SIZE; ++ii)
+    {
+            for (jj = 0; jj < SIZE; ++jj)
+            {
+                    if (ii == jj)
+                    {
+                            data[ii][jj] = data[jj][ii] = MAX;
+                    }
+                    else
+                    {
+                            dint tmp = (dint)rand() % MAX;
+                            data[ii][jj] = data[jj][ii] = tmp;
+                    }
+            }
+    }
+}
+
+void printGraph()
+{
+    unsigned ii, jj;
+    for (ii = 0; ii < SIZE; ++ii)
+    {
+            for (jj = 0; jj < SIZE; ++jj)
+            {
+                    if(data[ii][jj] == MAX)
+                            printf("%u ", 0);
+                    else
+                            printf("%u ", data[ii][jj]);
+            }
+            printf("\n");
+    }
+}
+
+unsigned gotAll()
+{
+    unsigned total = 0;
+    unsigned ii;
+    //#pragma omp parallel for reduction(+:total)
+    for (ii = 0; ii < SIZE; ++ii)
+    {
+	if (got[ii] == TRUE)
+		total += 1;
+    }
+    //printf("%u\n", total);
+    return total == SIZE;
+}
+
+int main()
+{
+    dint minimum = MAX;
+    unsigned x = 0, y = 0;
+    long long unsigned int mst = 0;
+    unsigned ii, jj;
+    generateGraph();
+    //printGraph();
+    
+    //#pragma omp parallel
+    {
+      unsigned lmin = MAX;
+      unsigned lx = 0, ly = 0;
+      
+      //#pragma omp for nowait
+      for (ii = 0; ii < SIZE; ++ii)
+      {
+	      for (jj = 0; jj < SIZE; ++jj)
+	      {
+		      if (data[ii][jj] < lmin)
+		      {
+			      lmin = data[ii][jj];
+			      lx = ii;
+			      ly = jj;
+		      }
+	      }
+      }
+      
+      //#pragma omp critical
+      {
+	if(lmin < minimum)
+	{
+	  minimum = lmin;
+	  x = lx;
+	  y = ly;
+	}
+      }
+    }
+    mst += data[x][y];
+    got[x] = TRUE;
+    got[y] = TRUE;
+    minimum = MAX;
+    //printf("%u %u\n", x + 1, y + 1);
+    
+    while (!gotAll())
+    {
+  //#pragma omp parallel
+	{
+	unsigned lmin = MAX;
+	unsigned lx = 0, ly = 0;
+	
+	//#pragma omp for nowait
+	for (ii = 0; ii < SIZE; ++ii)
+	{
+		for (jj = 0; jj < SIZE; ++jj)
+		{
+		    if ((got[ii] ^ got[jj]) && (data[ii][jj] < lmin))
+		    {
+			    lmin = data[ii][jj];
+			    lx = ii; 
+			    ly = jj;
+		    }
+		}
+	}
+	      
+	//#pragma omp critical
+	{
+	  if(lmin < minimum)
+	  {
+	    minimum = lmin;
+	    x = lx;
+	    y = ly;
+	  }
+	}
+      }
+      mst += data[x][y];
+      got[x] = TRUE;
+      got[y] = TRUE;
+      minimum = MAX;
+      //printf("%u %u\n", x + 1, y + 1);
+    }
+    printf("%llu", mst);
+}
+
+
